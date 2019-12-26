20867d8829fdfc77e493065a29029fd776f10c36
Amgad Malik <amgadmalik@yahoo.com>
2019-12-26 02:17:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,20 +7,17 @@ class App < Sinatra::Base
 
   get '/name' do
     @name = "My name is __"
-    # status 200
     # erb :'name/index.html.erb'
   end
   
   get '/hometown' do
     @hometown = "My hometown is __"
-    erb :'hometown/index.html.erb'
-    resp.status = 200
+    # erb :'hometown/index.html.erb'
   end
 
   get '/favorite-song' do
     @favorite_song = "My favorite song is __"
-    erb :'favorite_song/index.html.erb'
-    resp.status = 200
+    # erb :'favorite_song/index.html.erb'
   end
   
 end
